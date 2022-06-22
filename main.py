b31a371d9e11f67fccb186367ffa7fc128751b6d
castdrian <adrifcastr@gmail.com>
2022-06-22 09:49:17 +0200

    progress

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.boxlayout import BoxLayout
 from kivy.config import Config
 
 Config.set('graphics', 'window_state', 'maximized')
-
-class InventoryScreen(Screen):
-	pass
 
 class MainScreen(Screen):
 	def check_out_button(self):
@@ -14,11 +10,16 @@ class MainScreen(Screen):
 	def check_in_button(self):
 		self.manager.current = 'inv'
 
+class LoginScreen(Screen):
+	pass
+
+class PurchaseScreen(Screen):
+	pass
+
 class WindowManager(ScreenManager):
 	pass
 
 class reIMSApp(App):
 	title = 'reIMS - REHAU Inventory Management System'
-	pass
 
 reIMSApp().run()
