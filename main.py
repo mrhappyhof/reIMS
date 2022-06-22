0cbe8cdeb1f7c859ee4d206058f731c16cb21f43
castdrian <adrifcastr@gmail.com>
2022-06-22 13:22:00 +0200

    more ui

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import StringProperty
 from kivy.config import Config
 
 Config.set('graphics', 'window_state', 'maximized')
 
 class MainScreen(Screen):
 	def check_out_button(self):
+		App.get_running_app().mode = 0
+		self.manager.get_screen('inv').mode_label = 'Mode: Check-Out Items'
 		self.manager.current = 'inv'
 	def check_in_button(self):
+		App.get_running_app().mode = 1
+		self.manager.get_screen('inv').mode_label = 'Mode: Check-In Items'
 		self.manager.current = 'inv'
 
 class LoginScreen(Screen):
 	pass
 
-class PurchaseScreen(Screen):
-	pass
+class InventoryScreen(Screen):
+	mode_label = StringProperty()
 
 class WindowManager(ScreenManager):
 	pass
 
 class reIMSApp(App):
 	title = 'reIMS - REHAU Inventory Management System'
+	mode = None
 
 reIMSApp().run()
