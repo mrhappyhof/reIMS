a2303f5cdc533c04f8aa3128b56b79fe50b35083
castdrian <adrifcastr@gmail.com>
2022-06-23 13:50:13 +0200

    please

--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import StringProperty
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
 from kivy.config import Config
-from kivy.clock import Clock
+from kivy.lang import Builder
 from src.util import confirmation_popup
 from src.usb import listen_to_rfid
 
@@ -20,14 +24,24 @@ class MainScreen(Screen):
 		self.manager.current = 'inv'
 
 class LoginScreen(Screen):
-	def on_enter(self):
-		Clock.schedule_once(self.login)
+	def login(self):
+		self.clear_widgets()
 
-	def login(self, _):
+		lay0 = FloatLayout()
+		lay1 = AnchorLayout(anchor_x='center',anchor_y='top')
+		lay2 = AnchorLayout(anchor_x='center',anchor_y='bottom')
+		lay1.add_widget(Label(text='PLEASE LOG IN WITH YOUR KEYCARD',font_size= 0.7 * lay1.height,height=150,size_hint=(None, None)))
+		lay2.add_widget(Image(source='res/img/keycard.png',size_hint=(.7, .7)))
+		lay0.add_widget(lay1)
+		lay0.add_widget(lay2)
+		self.add_widget(lay0)
+	
 		user = listen_to_rfid()
 		if user != None: 
 			self.manager.get_screen('inv').login_label = f'LOGGED IN AS: {user}'
-			self.manager.current = 'main'
+			self.manager.current = 'main' 
+	def help(self):
+		pass
 
 class InventoryScreen(Screen):
 	mode_label = StringProperty()
